3346714e3ca0581b2e723b66dd47727f68b0d011
ANQIC <N9871047@qut.edu.au>
2019-09-29 14:23:20 +1000

    Modified AddPage
    AddPage, Select Picture from camera roll

--- Trady/Trady/Trady.iOS/AppDelegate.cs
+++ Trady/Trady/Trady.iOS/AppDelegate.cs
@@ -22,10 +22,19 @@ namespace Trady.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            SetIoc();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+        }
+        private void SetIoc()
+        {
+            var resolverContainer = new global::XLabs.Ioc.SimpleContainer();
+            resolverContainer
+                .Register<IMediaPicker, MediaPicker>();
+
+            XLabs.Ioc.Resolver.SetResolver(resolverContainer.GetResolver());
         }
     }
 }
