356b6d2ebeac4871769e4a887c097576c2bed7db
kongsyuenyeong <kongsyuen@outlook.com>
2019-09-30 16:29:43 +1000

    Savelist ItemCell

--- Trady/Trady/Trady/Views/SavelistPage.xaml.cs
+++ Trady/Trady/Trady/Views/SavelistPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Trady.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,65 @@ namespace Trady.Views
         public SavelistPage()
         {
             InitializeComponent();
+            listview.ItemsSource = LoadData();
         }
+        #region "LoadData"
+        protected IList<Item> LoadData()
+        {
+            var items = new List<Item>();
+            items.Add(new Item
+            {
+                Information = "Second Hand",
+                Category = "Accessories",
+                Date = "29.09.2019",
+                Photo = ImageSource.FromFile("A0001.png")
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "Adoption by caring people",
+
+                Category = "Pet",
+                Date = "29.09.2019",
+
+                Photo = ImageSource.FromFile("A0002.png")
+
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "Sell",
+
+                Category = "Transportation",
+                Date = "29.09.2019",
+                Photo = ImageSource.FromFile("A0003.png")
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "Second hand for sell",
+
+                Category = "Accessories",
+                Date = "29.09.2019",
+
+                Photo = ImageSource.FromFile("A0004.png")
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "New product sell",
+
+                Category = "Household",
+                Date = "29.09.2019",
+
+                Photo = ImageSource.FromFile("A0005.png")
+            });
+            return items;
+        }
+        #endregion
     }
 }
