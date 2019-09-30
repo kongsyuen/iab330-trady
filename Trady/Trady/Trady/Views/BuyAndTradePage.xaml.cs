cbad6c7ae95dfe655bec1a34b11513b2fb6ac182
ANQIC <N9871047@qut.edu.au>
2019-09-30 12:26:29 +1000

    detail page

--- Trady/Trady/Trady/Views/BuyAndTradePage.xaml.cs
+++ Trady/Trady/Trady/Views/BuyAndTradePage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Trady.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,7 @@ namespace Trady.Views
         public BuyAndTradePage()
         {
             InitializeComponent();
+            listview.ItemsSource = LoadData();
         }
         void OnSearchBarButtonPressed(object sender, EventArgs e)
         {
@@ -25,5 +26,68 @@ namespace Trady.Views
         {
             await Navigation.PushAsync(new AddPage());
         }
+        void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            var items = e.Item as Item;
+            Navigation.PushAsync(new DetailPage(items.Information, items.Date));
+        }
+        #region "LoadData"
+        protected IList<Item> LoadData()
+        {
+            var items = new List<Item>();
+            items.Add(new Item
+            {
+                Information = "Second Hand",
+                Category = "Accessories",
+                Date = "29.09.2019",
+                Photo = ImageSource.FromFile("A0001.png")
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "Adoption by caring people",
+
+                Category = "Pet",
+                Date = "29.09.2019",
+
+                Photo = ImageSource.FromFile("A0002.png")
+
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "Sell",
+
+                Category = "Transportation",
+                Date = "29.09.2019",
+                Photo = ImageSource.FromFile("A0003.png")
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "Second hand for sell",
+
+                Category = "Accessories",
+                Date = "29.09.2019",
+
+                Photo = ImageSource.FromFile("A0004.png")
+            });
+
+            items.Add(new Item
+            {
+
+                Information = "New product sell",
+
+                Category = "Household",
+                Date = "29.09.2019",
+
+                Photo = ImageSource.FromFile("A0005.png")
+            });
+            return items;
+        }
+        #endregion
     }
 }
---
+++ Trady/Trady/Trady/Views/DetailPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Trady.Views
+{
+    public partial class DetailPage : ContentPage
+    {
+        private string date;
+
+        public DetailPage(string information)
+        {
+            InitializeComponent();
+        }
+
+        public DetailPage(string information, string date) : this(information)
+        {
+            this.date = date;
+        }
+    }
+}
