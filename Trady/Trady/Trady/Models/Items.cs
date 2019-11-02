8b71532bd51006bd2a315209cf068c66a605fedb
ANQIC <N9871047@qut.edu.au>
2019-11-03 01:35:25 +1000

    Update Items.cs

--- Trady/Trady/Trady/Models/Items.cs
+++ Trady/Trady/Trady/Models/Items.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using Xamarin.Forms;
+﻿//using System;
+//using Xamarin.Forms;
 
-namespace Trady.Models
-{
-    public class Items
-    {
-        public Items()
-        {
-        }
+//namespace Trady.Models
+//{
+//    public class Items
+//    {
+//        public Items()
+//        {
+//        }
 
-        public string Information { get; set; }
-        public ImageSource Photo { get; set; }
-        public string Category { get; set; }
-        public string Date { get; set; }
-    }
-}
+//        public string Information { get; set; }
+//        public ImageSource Photo { get; set; }
+//        public string Category { get; set; }
+//        public string Date { get; set; }
+//    }
+//}
