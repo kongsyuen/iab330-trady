288fd15993e15dda7f6d5c0016ac8e26d2e0d8a8
kongsyuen <48376665+kongsyuen@users.noreply.github.com>
2019-11-03 23:39:46 +1000

    Update

--- Trady/Trady/Trady/Views/EditPage.xaml.cs
+++ Trady/Trady/Trady/Views/EditPage.xaml.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Trady.Interface;
+using Trady.Models;
 using Xamarin.Forms;
+using XLabs.Ioc;
 
 namespace Trady.Views
 {
     public partial class EditPage : ContentPage
     {
-        public EditPage()
+        IItemRepository itemRepository;
+        public EditPage(Item item)
         {
             InitializeComponent();
+            itemRepository = Resolver.Resolve<IItemRepository>();
+            BindingContext = item;
+        }
+        
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            var item = BindingContext as Item;
+            itemRepository.Update(item);
+            await Navigation.PopAsync();
         }
     }
 }
